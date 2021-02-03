4fb94e98c8ed064cced1212a2ca16d34549f899b
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-03 22:37:47 +0000

    Fixing 101file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
-main(void)
-}
+
+int main(void)
+{
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(2, str, strlen(str));
+
+	write(2, str, 59);
 	return (1);
 }
-
