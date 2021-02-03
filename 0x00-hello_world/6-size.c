90eb1c3196308303a23d3c1666a8e1364c9acc52
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-03 18:42:23 +0000

    Fixing 6-size file, removing an s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	printf("Size of a char: %zd bytes(s)\n", sizeof(char));
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
 	printf("Size of an int: %zd byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
