cbc31e1232a0e74bdeb48800941b9550f4b00676
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-03 18:38:21 +0000

    Fixing 6-size file, betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> 
-
+#include <stdlib.h>
 
 /**
 * main - Entry point
@@ -10,11 +9,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %zd bytes(s)\n",sizeof(char));
-	printf("Size of an int: %zd byte(s)\n",sizeof(int));
-	printf("Size of a long int: %zd byte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %zd byte(s)\n",sizeof(long long int));
-	printf("Size of a float: %zd byte(s)\n",sizeof(float));
+	printf("Size of a char: %zd bytes(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
 	return (0);
 }
 
