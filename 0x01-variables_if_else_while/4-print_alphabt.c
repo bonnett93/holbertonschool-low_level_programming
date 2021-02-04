b8278ddf280d95e0b6c4b6cd012b57c75c76e9eb
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-04 21:48:14 +0000

    upload 4 file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+			putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
