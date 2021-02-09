aa3aeefa2e7769d23207804a5e34bebf5b3c3255
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-09 00:00:03 +0000

    Upload file 0-holberton.c, holberton.h, README.md

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char text[] = "Holberton";
+	int i = 0;
+	
+	while (i < 10)
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# C - Functions, nested loops
+
+In this directory we found code about:
+
+-Nested while loops
+- C - Functions
+- Function prototype?
+- C - Header Files
