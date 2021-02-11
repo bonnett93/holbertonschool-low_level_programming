a146a62ff9f947b2aebb16fdee69ed1636bdd8cc
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-11 04:37:41 +0000

    Fixing header file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,13 +1,20 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 #include <stdio.h>
-
 void positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
-
-int convert_day(int month, int day);
-
-void print_remaining_days(int month, int day, int year);
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+int largest_number(int a, int b, int c);
+#endif
+
+#ifndef CONVERT_DAY
+#define cONVERT_DAY
+int convert_day(int month, int day);
+#endif
+
+#ifndef PRINT_REMAINING_DAYS
+#define PRINT_REMAINING_DAYS
+void print_remaining_days(int month, int day, int year);
+#endif
