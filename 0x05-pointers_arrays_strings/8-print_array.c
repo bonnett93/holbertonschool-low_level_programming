df82e02d1f9c89ab8342bb17788d0ac856aa01be
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-17 00:59:03 +0000

    Upload 8-file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print a string to stdout.
+ * @a: string to be printed in reverse.
+ * @n: the number of elements of the array to be printed.
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", *a);
+		a++;
+	}
+	
+	printf("%d\n", *a);
+}
+
