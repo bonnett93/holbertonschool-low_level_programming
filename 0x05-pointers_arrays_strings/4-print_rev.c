907e5684e0476c3d6ca24bea03a038f64d397697
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-17 00:37:09 +0000

    upgrade 4 file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,14 @@
 void print_rev(char *s)
 {
 	int c = 0;
-	int i;
 
 	while (*(s + c) != '\0')
 	{
 		c++;
 	}
-	for (i = c; i >= 0; i--)
+	for (; c >= 0; c--)
 	{
-		_putchar(*(s + i));
+		_putchar(*(s + c));
 	}
 	_putchar('\n');
 }
