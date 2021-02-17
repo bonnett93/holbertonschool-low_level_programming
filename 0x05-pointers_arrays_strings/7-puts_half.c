e8408fbb8117e23dfbda95f2d6f8d9fc0a458f49
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-17 00:53:11 +0000

    Upload 7 file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print a string to stdout.
+ * @str: string to be printed in reverse.
+ *
+ * Return: void.
+ */
+
+void puts_half(char *str)
+{
+	int c = 0;
+	int i;
+
+	while (*(str + c) != '\0')
+	{
+		c++;
+	}
+
+	for (i = (c + 1) / 2; i <= c; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
+
