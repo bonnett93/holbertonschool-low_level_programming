deba117d960923118f55893691ef3af963cc0d5a
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-17 00:45:43 +0000

    Fixing 4 file, count - 1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@ void print_rev(char *s)
 {
 	int c = 0;
 
-	while (*(s + c) != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
 	}
-	for (; c >= 0; c--)
+	for (c = c - 1; c >= 0; c--)
 	{
-		_putchar(*(s + c));
+		_putchar(s[c]);
 	}
 	_putchar('\n');
 }
