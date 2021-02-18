3836dd99b84708cc886a5a1adf9e5c3c1afeab70
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-18 20:32:20 +0000

    Upload 3 file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compare two strings.
+ * @s1: First string to compare.
+ * @s2: Second strgin to compare.
+ *
+ * Return: Integer, sustraccion s1 - s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int suma = 0;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			suma = s1[i] - s2[i];
+			break;
+		}
+		
+	}
+	return suma;
+}
