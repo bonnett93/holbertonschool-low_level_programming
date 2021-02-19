da69c3052e396258a169a4fd4410e1b8c62d2e9e
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-19 04:12:25 +0000

    Upgrade 8 file, fixing 1 if stament

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,15 +9,22 @@
 
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
+	char *lett = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int numb[] = {110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+			97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+			78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+			65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77};
 
 	for (i = 0; str[i]; i++)
 	{
-
-		if ((str[i] >= 'A' && str[i] <= 'M')
-			|| (str[i] >= 'a' && str[i] <= 'm'))
+		for (j = 0; lett[j]; j++)
 		{
-			str[i] = str[i] + 13;
+			if (str[i] == lett[j])
+			{
+				str[i] = numb[j];
+				break;
+			}
 		}
 	}
 	return (str);
