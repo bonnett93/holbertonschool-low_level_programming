e6b30c3faa02d92661f888c1b07fdfb352f5c42e
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-22 23:03:11 +0000

    upload 1-file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * *_mecpy - Copies memory area.
+ * @dest: A pointer to the block of memory to copy in.
+ * @src: A pointer to the block of memory to copy.
+ * @n: Size of the memory block to copy.
+ *
+ * Return: A pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
