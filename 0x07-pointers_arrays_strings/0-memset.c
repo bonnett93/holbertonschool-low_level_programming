2cc83d0443ea0f4973864869dea4788f47a52c26
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-22 22:49:30 +0000

    Upload README.md holberton.h 0-file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: A pointer to the block of memory.
+ * @b: Constant byte to fill with.
+ * @n: Size of the memory block to fill.
+ *
+ * Return: A pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for(i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
