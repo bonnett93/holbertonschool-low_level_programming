b4f5cd91d8b29adc1d9a29946449779d008c5033
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-24 04:10:27 +0000

    Upload 9 file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - print the sum ot 2 diagonals.
+ * @s: Pointer to pointer char.
+ * @to: pointer to char.
+ *
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
