c2391a3b88e9e68492c14bb1b25eb53428a86697
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-24 17:07:45 +0000

    Fixing 1,2,3 files, betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,11 +4,11 @@
  * _strlen_recursion - returns the length of a string
  * @s: The string
  *
- * Return: int, length of s. 
+ * Return: int, length of s.
  */
 int _strlen_recursion(char *s)
 {
-	if(!s[0])
+	if (!s[0])
 	{
 		return (0);
 	}
