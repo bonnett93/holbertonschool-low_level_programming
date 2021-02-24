4737a97b0ed9097d2db7d108dd87ccf45ecd6ec2
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-24 03:44:50 +0000

    Upload 8 file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum ot 2 diagonals.
+ * @a: pointer to matrix start.
+ * @size: the size of matrix.
+ *
+ * Return: void.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, dig1, dig2;
+
+	dig1 = 0, dig2 = 0;
+
+	for (i = 0; i < size * size; i += size + 1)
+	{
+		dig1 += a[i];
+	}
+	i -= size * 2;
+	for (; i >= 0; i -= size - 1)
+	{
+		dig2 += a[i];
+	}
+	printf("%d, %d\n", dig1, dig2);
+}
