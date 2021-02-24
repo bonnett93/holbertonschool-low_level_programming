1c090d6e0d07cd7a328c3779a0431941b6d7403f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-02-24 21:11:16 +0000

    Upgrade holberton.h and 5 files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 
-int _sqrt_decrement(int, int);
 /**
  * _sqrt_recursion - returns the natural square root of n.
  * @n: positive int.
@@ -10,14 +9,12 @@ int _sqrt_decrement(int, int);
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-	{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
 		return (0);
-	}
 	else
-	{
 		return (_sqrt_decrement(n, n - 1));
-	}
 }
 
 /**
