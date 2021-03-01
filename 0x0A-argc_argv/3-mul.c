aabe9d84f7b56f22de1be3e42ec4dd631fb0b477
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-01 01:49:21 +0000

    Upload 3 file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: An integer 0 if succes
+*/
+
+int main(int argc, char *argv[])
+{
+	int rest;
+
+	if (argc == 3)
+	{
+		rest = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", rest);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
