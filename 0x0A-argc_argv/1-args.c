1eac7dd9d2acd0652089912d633a85c7cb5ec3cd
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-01 01:05:09 +0000

    Upload 1-file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ *
+ * Return: An integer 0 if succes
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
