194ceed41f68886024b6a608f01860a0138441f8
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-02 21:43:37 +0000

    fixing 0-file, NULL if size == 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return (NuLL);
+		return (0);
 
 	ptr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
