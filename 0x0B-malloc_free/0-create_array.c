f84c1094feb7e056af65f3a558d41574f0d026a7
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-02 21:39:07 +0000

    Upload README, header, 0-file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars.
+ * @size: Size of array
+ * @c: Char to initialize the array
+ *
+ * Return: Ponter to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+# 0x0B. C - malloc, free
+
+Part of Holberton school program
