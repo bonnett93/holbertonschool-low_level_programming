5a126f16e49cb4ce492ea6e715efdc33594b2dbf
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-03 00:16:51 +0000

    Fixing 2 file, if str = NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 	int i, j;
-	int len = 0;
+	int len = 1;
+
+	if (s1 == 0)
+		s1 = "";
+
+	if (s2 == 0)
+		s2 = "";
 
 	for (i = 0; s1[i]; i++)
 		len++;
