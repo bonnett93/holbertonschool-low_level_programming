751c927389b8f332620bc366e03866b8782c017c
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-03 00:03:16 +0000

    Upload 2nd file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+ * *str_concat - Concatenate 2 strings.
+ * @s1: First string
+ * @s1: Second string
+ *
+ * Return: Ponter to array
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, j;
+	int len = 1;
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	for (i = 0; s2[i]; i++)
+		len++;
+
+	ptr = malloc(sizeof(char) * len);
+	if (ptr == 0)
+		return (0);
+
+	for (i = 0; s1[i]; i++)
+		ptr[i] = s1[i];
+
+	for (j = 0; s2[j]; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
