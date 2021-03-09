d0b382182bf32f3e758702f357b5325b998dd69f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-09 04:55:03 +0000

    Upload 4 file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
 
-char *cp_str(char *str, int len);
-
 /**
 * *new_dog -  creates a new dog.
 *
@@ -15,50 +13,14 @@ char *cp_str(char *str, int len);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, j;
 
 	dog = malloc(sizeof(struct dog));
 	if (dog != 0 && name != 0 && owner != 0)
 	{
-		for (i = 0; name[i]; i++)
-			continue;
-		for (j = 0; owner[j]; j++)
-			continue;
-		dog->name = cp_str(name, i);
-		if (dog->name == 0)
-		{
-			free(dog);
-			return (0);
-		}
+		dog->name = name;
 		dog->age = age;
-		dog->owner = cp_str(owner, j);
-		if (dog->owner == 0)
-		{
-			free(dog->name);
-			free(dog);
-			return (0);
-		}
+		dog->owner = owner;
 		return (dog);
 	}
 	return (0);
-}
-
-/**
- * cp_str - return a copy
- * @str: string
-¨* @len: length
- * Return: char
-*/
-char *cp_str(char *str, int len)
-{
-	char *new_str;
-	int i;
-
-	new_str = malloc(len);
-	if (new_str == 0)
-		return (0);
-
-	for (i = 0; i < len; i++)
-		new_str[i] = str[i];
-	return (new_str);
 }
