222dba5bc79eb119cb52e0e5728a7f52a4281bbd
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-09 01:09:53 +0000

    Upload header and README.md

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+0x0E. C - Structures, typedef
+
+Part of HS program
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Create a dog basic documentation
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dogś owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
