62926189afa7083fe6037f1875a32afbb80433c3
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-09 02:51:15 +0000

    Upload 2 file and upgrader header dog.h with new function print_dog()

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_dog - prints a struct dog
+*
+* @d: The adress (pointer) struct to print
+* Return: Void.
+*/
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		if (!d->age)
+			printf("(nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
