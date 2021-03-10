43e01b09f31238ce344362295d1ebb9b3e443f49
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-10 18:26:16 +0000

    Upgrade 1-file, if arg == 0 return, fixing betty style adding description

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,16 @@
 #include <stddef.h>
 /**
- * print_name - Prints a name
- * @name: the name to print
- * @f: A function that print the name
+ * array_iterator - executes a function given on each element of an array.
+ * @array: the array
+ * @size: number of elements in array
+ * @action: the function to do
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
+	if (array == 0 || size == 0 || action == 0)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
