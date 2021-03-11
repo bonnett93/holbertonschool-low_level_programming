81b1d8adc5c752d197712195f00d6a773ade7140
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-11 04:06:46 +0000

    Upgrade 3-main, inf argv[2][1]== 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,8 +21,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' &&
-		*argv[2] != '/' && *argv[2] != '%')
+	if (argv[2][1] == 0)
 	{
 		printf("Error\n");
 		exit(99);
