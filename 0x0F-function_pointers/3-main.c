962d82559e6213be3d1bb8e5e711cb654ea47578
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-11 02:19:05 +0000

    Upload the files to do calculator of task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ * main - performs aritmetic operations from terminal
+ * @argc: the number of arg passed
+ * @argv: array of arguments
+ *
+ * Return: 0 if succes
+ * exit 98 if number of arguments ar wrong.
+ * exit 99 if the operator is incorrect.
+ * exit 100 if user tris to divide by 0.
+*/
+int main(int argc, char *argv[])
+{
+	int answer;
+
+	if (argc != 4)
+		exit(98);
+
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' &&
+		*argv[2] != '/' && *argv[2] != '%')
+		exit(99);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+		exit(100);
+
+	answer = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", answer);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: the result
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - find the module of two numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
