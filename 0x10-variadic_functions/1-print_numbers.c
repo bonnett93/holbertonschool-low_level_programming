e09956a44ff0c62f95d20770b3b8a1aeac0fb65f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-11 19:51:49 +0000

    Fixing 1-file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list lista;
 
-	if (n == 0 || *separator == NULL)
+	if (n == 0 || separator[0] == NULL)
 		return;
 
 	va_start(lista, n);
