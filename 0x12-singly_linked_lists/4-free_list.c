445b4e06a2f88ede95d7f447c6d1b9911e0996b3
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-19 23:19:18 +0000

    Upload 4-file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include"lists.h"
+/**
+ * free_list - Frees a list.
+ *
+ * @head: Pointer to list head.
+ * Return: void.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while(head!= NULL)
+	{
+		tmp = head;
+		head = head->next;
+		if (tmp->str != NULL)
+			free(tmp->str);
+		free(tmp);
+	}
+}
