5869b5c6a6d63218a61f8a674ed1643b36378f85
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-20 02:11:44 +0000

    Upload 100-file, upgrade lists header include 100-file function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_first - print text before main execute.
+ * Return: Void.
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
