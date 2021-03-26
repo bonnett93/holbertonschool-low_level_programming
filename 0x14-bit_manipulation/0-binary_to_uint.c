0b5727cd344cd698b5510584753e509fcb5a22f3
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-26 15:07:07 +0000

    Upgrade 0*.c File

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,39 +17,24 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[i])
 	{
-		if (b[i] != 48 && b[i] != 49)
+		if (b[i] != 48 && b[i] != 49 && b[i] != 32)
 			return (0);
 		i++;
 	}
+
 	i--;
 	decimal += b[i] - 48;
 	i--;
+
 	while (i >= 0)
 	{
-		if (b[i] != 48)
-			decimal += pot(p);
-		p++;
+		if (b[i] != 32)
+		{
+			decimal += (b[i] - 48) * (1 << p);
+			p++;
+		}
 		i--;
 	}
 
 	return (decimal);
-}
-
-/**
- * pot - Calculates 2**p.
- * @p: Potenciator.
- * Return: 2**p.
- */
-
-unsigned int pot(unsigned int p)
-{
-	unsigned int potencia = 1;
-
-	while (p != 0)
-	{
-		potencia *= 2;
-		p--;
-	}
-
-	return (potencia);
 }
