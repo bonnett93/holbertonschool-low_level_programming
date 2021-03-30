9e074d0954e64385a467e4f8eeb9c4452f1ab21c
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-30 01:50:58 +0000

    Upload 0*.c file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: The file to read.
+ * @letters: the number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int buffer[2148];
+	ssize_t bytes_read, bytes_write;
+
+	if (!filename || filename[0] == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	close(fd);
+
+	bytes_write = write(1, buffer, bytes_read);
+	if (bytes_read != bytes_write)
+		return (0);
+
+	return (bytes_read);
+}
