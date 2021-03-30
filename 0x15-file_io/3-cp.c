91978a3ac4131e6176c02662b8f955fe0f3805ac
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-30 04:56:12 +0000

    File 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,20 +19,20 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
 	byte_read = read(fd1, buffer, 1048);
 	if (fd1 == -1 || byte_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	close_1 = close(fd1);
 	if (close_1 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d", fd1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
 		exit(100);
 	}
 	fd2 = open(argv[2], O_RDWR | O_CREAT | O_EXCL, 0664);
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 	close_2 = close(fd2);
 	if (close_2 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
 		exit(100);
 	}
 	return (0);
