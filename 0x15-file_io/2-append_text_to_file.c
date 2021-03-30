224e7aa3151b81e9fda3ce6110406b8868c28722
Jordan Bonnett <bonnettjordan@gmail.com>
2021-03-30 14:53:55 +0000

    Fixing 2 file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_RDWR);
 	if (fd == -1)
 		return (-1);
-
+	if (text_content == NULL)
+		return (1);
 	byte_write = write(fd, text_content, strlen(text_content));
 	if (byte_write == -1)
 		return (-1);
