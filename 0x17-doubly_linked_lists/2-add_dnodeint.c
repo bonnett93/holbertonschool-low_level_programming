e4a5f757c0dd38a81c93a5cb82535510f4bcd44f
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-02 19:19:49 +0000

    Upgrade 2-file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,11 +11,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return (0);
+		return (NULL);
 	new->n = n;
 	new->prev = NULL;
 	if (*head != NULL)
