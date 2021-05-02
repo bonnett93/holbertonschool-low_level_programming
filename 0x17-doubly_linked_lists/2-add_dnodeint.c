9b7fd1196d60f19ee2269ba12546d1f03effa358
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-02 19:16:56 +0000

    Fixing, 2-file, if *head != NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @h: Pointer to dlistint_t list &head.
+ * @head: Pointer to dlistint_t list &head.
  * @n: element of the node.
  * Return: the address of the new element, or NULL if it failed.
  */
@@ -18,6 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (0);
 	new->n = n;
 	new->prev = NULL;
+	if (*head != NULL)
+		new->prev = (*head)->prev;
 	new->next = *head;
 	*head = new;
 
