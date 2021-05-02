573442eee5e468cceda245d49c1d2298bfdd87f5
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-02 22:57:52 +0000

    FIXING, 3-file, if head ==NULL

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,12 +8,14 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *tmp = *head;
+	dlistint_t *new, *tmp;
 
 	if (head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
@@ -22,8 +24,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new->prev = NULL;
 		return (new);
 	}
+	tmp = *head;
 	while (tmp->next != NULL)
+	{
 		tmp = tmp->next;
+	}
 	tmp->next = new;
 	new->prev = tmp;
 
