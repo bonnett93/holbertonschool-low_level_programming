99396d458b314be54224bf933d1a4ab804c58e1e
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-30 07:39:02 +0000

    Fix,3-file: change nidex position

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *tmp;
 	int control = 0;
 
-	index = key_index((const unsigned char *)key, ht->size);
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp != NULL)
 	{
