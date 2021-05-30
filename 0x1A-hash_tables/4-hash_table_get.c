e8578cbe50d82f8d08372a1afa59ea2d43653fc4
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-30 08:10:32 +0000

    Upload 4-file, Upgrade header

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get -  retrieves a value associated with a key.
+ * @ht: The hash table you want to add or update the key/value to
+ * @key: The key you are looking for
+ * Return: The value of key, NULL if key doesn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key[0] == '\0')
+		return NULL;
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while(tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,4 +45,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 int add_node(hash_node_t **head, const char *key, const char *value);
 
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif
