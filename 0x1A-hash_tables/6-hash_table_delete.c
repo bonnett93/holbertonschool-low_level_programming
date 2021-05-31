b1504d758e5946190f2105171f2d49565c534839
Jordan Bonnett <bonnettjordan@gmail.com>
2021-05-31 06:19:25 +0000

    Upload 6-file, Upgrade header

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Delete a hash table
+ * @ht: The hash table you want to add or update the key/value to
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp = NULL, *delete = NULL;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	(void)tmp;
+	(void)delete;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while(tmp)
+			{
+				delete = tmp;
+				tmp = tmp->next;
+				free(delete->key);
+				free(delete->value);
+				free(delete);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
