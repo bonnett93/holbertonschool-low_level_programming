0bb3853f8c6b6d177a0eb998bae72276fe9acbe7
Jordan Bonnett <bonnettjordan@gmail.com>
2021-06-02 17:30:00 +0000

    Try_0-file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = NULL;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(size * 8);
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 
