96f2eb32cb6fe31742628fde387a3b1a185bd615
Jordan Bonnett <bonnettjordan@gmail.com>
2021-07-05 02:51:15 +0000

    Change 5 file, ranges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,15 +4,31 @@
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
-    for r in range(1, len(grid) - 1):
-        for c in range(1, len(grid[r]) - 1):
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
             if grid[r][c] == 1:
-                if grid[r][c - 1] == 0:
-                    perimeter += 1
-                if grid[r][c + 1] == 0:
-                    perimeter += 1
-                if grid[r - 1][c] == 0:
-                    perimeter += 1
-                if grid[r + 1][c] == 0:
-                    perimeter += 1
+                if c != 0:
+                    if grid[r][c - 1] == 0:
+                        perimeter += 1
+                else:
+                    if grid[r][c] == 1:
+                        perimeter += 1
+                if c != (len(grid[r]) - 1):
+                    if grid[r][c + 1] == 0:
+                        perimeter += 1
+                else:
+                    if grid[r][c] == 1:
+                        perimeter += 1
+                if r != 0:
+                    if grid[r - 1][c] == 0:
+                        perimeter += 1
+                else:
+                    if grid[r][c] == 1:
+                        perimeter += 1
+                if r != len(grid) -1 :
+                    if grid[r + 1][c] == 0:
+                        perimeter += 1
+                else:
+                    if grid[r][c] == 1:
+                        perimeter += 1
     return perimeter
